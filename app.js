38bc7c7c55edf97b04f37f418abcbf19c49f8f31
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-05-25 22:07:53 -0700

    Functionality  done

--- app.js
+++ app.js
@@ -5,35 +5,35 @@ $(document).ready(function () {
         {
             question: "What is the largest continent on planet Earth?",
             choices: ["Africa", "Asia", "America", "Australia"],
-            correct: 2,
+            correct: 1,
             correctDetails: "Asia is 30% of the world in area."
                 },
 
         {
             question: "Which continent has the most countries?",
             choices: ["Africa", "Europe", "Asia", "America"],
-            correct: 1,
+            correct: 0,
             correctDetails: "The continent of Africa has 53 countries."
                 },
 
         {
             question: "How many countries are there on planet Earth?",
             choices: ["78", "116", "154", "196"],
-            correct: 4,
+            correct: 3,
             correctDetails: "There are 196 countries, including Taiwan."
                 },
 
         {
             question: "What is the largest country (in area) in the world?",
             choices: ["Australia", "India", "Russia", "China"],
-            correct: 3,
+            correct: 2,
             correctDetails: "Russia has a land mass of 17,075,200 square km."
                 },
 
         {
             question: "Which country has the greates bio-diversity of flora and fauna on Earth?",
             choices: ["Madagascar", "Brazil", "Indonesia", "United States"],
-            correct: 2,
+            correct: 1,
             correctDetails: "Brazil has more than....."
                 }
 
@@ -55,7 +55,7 @@ $(document).ready(function () {
     /*--- Display Questions Function ---*/
     function questionDisplay() {
         //displays the current question
-        $("#question-number").text("Question" + (questionNum + 1) + " of " + questionTotal);
+        $(".question-number").text("Question " + (questionNum + 1) + " of " + questionTotal);
         $("#question").text(questions[questionNum].question);
         $('#choices').empty();
         var choiceTotal = questions[questionNum].choices.length;
@@ -67,7 +67,7 @@ $(document).ready(function () {
 
     /*--- On start quiz ---*/
     $(".start-button").click(function () { //start the quiz and show the first question
-        $("start-section").hide;
+        $(".start-section").hide();
         $(".quiz-section").show();
         $(".result-section").hide();
         //empty the result details container
@@ -81,21 +81,21 @@ $(document).ready(function () {
         var correctAnswer = questions[questionNum].correct;
 
         if (answer == correctAnswer) {
-            correctTotal += 1;
+            correctTotal++;
         }
 
         //quiz is finished, show result-section
         if ((questionNum + 1) == questionTotal) {
             $("#finalScore").text(correctTotal + "/" + questionTotal);
 
-            $(".start-button").show()
+            $(".start-button").show();
             $("start-section").hide;
             $(".quiz-section").hide();
             $(".result-section").show();
 
         } else {
             //continue to next question
-            questionNum += 1;
+            questionNum++;
             questionDisplay();
         }
 
@@ -103,7 +103,7 @@ $(document).ready(function () {
 
     /*--- Load the start section from the result section ---*/
     $(".result-section").on("click", "#tryagain", function () {
-        $("start-section").show;
+        $(".start-section").show();
         $(".quiz-section").hide();
         $(".result-section").hide();
         //reset variables to start quiz again
